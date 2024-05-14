77a390cdbd2a07c8622e87db551f6c5b17733c6e
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-14 14:27:14 -0300

    first commit

---
+++ src/test/java/com/login/springlogin/SpringloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.springlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
