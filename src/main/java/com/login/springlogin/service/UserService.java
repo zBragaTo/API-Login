13be96feaaf4bc0ecccd4b6ce42dfe2d9c8a8ab2
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-16 20:26:51 -0300

    Criptografando Password

--- src/main/java/com/login/springlogin/service/UserService.java
+++ src/main/java/com/login/springlogin/service/UserService.java
@@ -1,8 +1,25 @@
 package com.login.springlogin.service;
 
 import org.springframework.stereotype.Service;
+import com.login.springlogin.repositories.UserRepository;
+import com.login.springlogin.models.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Service
 public class UserService {
-    
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User saveUser(User user){
+        String senhaCriptografada = passwordEncoder.encode(user.getPassword());
+        user.setPassword(senhaCriptografada);
+        return userRepository.save(user);
+    }
+
 }
