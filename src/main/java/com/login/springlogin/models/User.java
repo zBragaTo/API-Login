ceb007b5e631bbc1bb9cf5f6a33bf3734b2740c1
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-16 19:46:55 -0300

    Creating User Model

--- src/main/java/com/login/springlogin/models/User.java
+++ src/main/java/com/login/springlogin/models/User.java
@@ -1,8 +1,28 @@
 package com.login.springlogin.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "User")
+@Getter
+@Setter
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String user;
+
+    private String email;
+
+    private String password;
+
 }
