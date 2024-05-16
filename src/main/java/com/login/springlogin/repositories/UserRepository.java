050a3fc91b9c94478ac646f15bc78fe888524eb1
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-16 19:54:41 -0300

    Putting Service and Repository

---
+++ src/main/java/com/login/springlogin/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.login.springlogin.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.login.springlogin.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
---
+++ src/main/java/com/login/springlogin/service/UserService.java
@@ -0,0 +1,8 @@
+package com.login.springlogin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    
+}
