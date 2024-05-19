92aa402ecd1413567db93f033814c1815d493808
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-18 22:11:32 -0300

    Create User Controller

---
+++ src/main/java/com/login/springlogin/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.login.springlogin.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.login.springlogin.models.User;
+import com.login.springlogin.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUser(@PathVariable String email){
+        return userService.findEmail(email).
+        map(ResponseEntity::ok).
+        orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        User newUser = userService.saveUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+}
