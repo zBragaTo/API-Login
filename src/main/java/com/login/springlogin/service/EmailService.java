2bbc250a6cc5ecedef8fe0da2f2961342061a7bb
zBragaTo <leonardobragatobassi@hotmail.com>
2024-05-30 23:01:20 -0300

    Putting Configuration EMAIL

---
+++ src/main/java/com/login/springlogin/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.login.springlogin.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    private final JavaMailSender emailSender;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+    
+}
