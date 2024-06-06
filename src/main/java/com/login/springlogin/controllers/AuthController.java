f8ec5171c97a9dfe7993af66463143cf5aad7104
zBragaTo <leonardobragatobassi@hotmail.com>
2024-06-05 23:16:03 -0300

    Organizing Code

--- src/main/java/com/login/springlogin/controllers/AuthController.java
+++ src/main/java/com/login/springlogin/controllers/AuthController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +34,24 @@ public class AuthController {
         String verificationLink = "http://localhost:8080/api/users/verify?token=" + token;
         emailService.sendSimpleMessage(newUser.getEmail(), "Verificação de Email", "Clique no link para verificar seu email: " + verificationLink);
         return ResponseEntity.ok("Registro bem-sucedido. Verifique seu email para ativar sua conta.");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+            try {
+        boolean emailChanged = userService.updateUser(id, updatedUser);
+        if (emailChanged) {
+            User user = userService.findById(id); 
+            String token = JwtTokenUtil.generateEmailVerificationToken(user.getEmail());
+            String verificationLink = "http://localhost:8080/api/users/verify?token=" + token;
+            emailService.sendSimpleMessage(user.getEmail(), "Verificação de Email", "Clique no link para verificar seu email: " + verificationLink);
+            return ResponseEntity.ok("Usuário atualizado com sucesso. \n Verifique seu novo e-mail para ativar sua conta.");
+        } else {
+            return ResponseEntity.ok("Usuário atualizado com sucesso.");
+        }
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping("/verify")
--- src/main/java/com/login/springlogin/controllers/UserController.java
+++ src/main/java/com/login/springlogin/controllers/UserController.java
@@ -11,16 +11,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.login.springlogin.dto.response.UserDTO;
 import com.login.springlogin.models.User;
-import com.login.springlogin.service.EmailService;
 import com.login.springlogin.service.UserService;
-import com.login.springlogin.token.JwtTokenUtil;
 
 @RestController
 @RequestMapping("/api/users")
@@ -29,9 +25,6 @@ public class UserController {
     
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private EmailService emailService;
 
     @GetMapping("/{email}")
     public ResponseEntity<User> getUser(@PathVariable String email){
@@ -44,24 +37,6 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> getAllUsers(){
         List<UserDTO> users = userService.findAll();
         return ResponseEntity.ok(users);
-    }
-
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
-            try {
-        boolean emailChanged = userService.updateUser(id, updatedUser);
-        if (emailChanged) {
-            User user = userService.findById(id); 
-            String token = JwtTokenUtil.generateEmailVerificationToken(user.getEmail());
-            String verificationLink = "http://localhost:8080/api/users/verify?token=" + token;
-            emailService.sendSimpleMessage(user.getEmail(), "Verificação de Email", "Clique no link para verificar seu email: " + verificationLink);
-            return ResponseEntity.ok("Usuário atualizado com sucesso. \n Verifique seu novo e-mail para ativar sua conta.");
-        } else {
-            return ResponseEntity.ok("Usuário atualizado com sucesso.");
-        }
-    } catch (IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
     }
 
     @DeleteMapping("/{id}")
